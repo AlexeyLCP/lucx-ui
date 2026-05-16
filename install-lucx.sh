6fe6818773124ff71c1acf9d3d8af26a6aa2478e
LucX-UI <lcp@lucx-ui.local>
2026-05-16 13:29:57 +0300

    fix(install): accept pre-release tags, lower min version to v0.1.0

--- install-lucx.sh
+++ install-lucx.sh
@@ -903,10 +903,10 @@ install_x-ui() {
     else
         tag_version=$1
         tag_version_numeric=${tag_version#v}
-        min_version="2.3.5"
+        min_version="0.1.0"
 
-        if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
+        if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" && "${tag_version_numeric}" != *"-"* ]]; then
+            echo -e "${red}Please use a newer version (at least v0.1.0). Exiting installation.${plain}"
             exit 1
         fi
 
