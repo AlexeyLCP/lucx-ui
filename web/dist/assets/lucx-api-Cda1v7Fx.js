116c14dda3ce211f40c62f954ad5e075cbc58a66
LucX-UI <lcp@lucx-ui.local>
2026-05-16 08:26:03 +0300

    chore: gitignore web/dist and release tarball

--- web/dist/assets/lucx-api-Cda1v7Fx.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./vendor-axios-B39MwfDK.js";var t=`/panel/api/lucx`;async function n(n,r){let{data:i}=await e.post(t+n,r);return i}export{n as t};
-//# sourceMappingURL=lucx-api-Cda1v7Fx.js.map
